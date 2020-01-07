3a34faa2d6c09915b2fcca6a48df703d2d8731b6
Amritansh <asingh64.corp@intuit.net>
2020-01-07 20:24:41 +0530

    Updates

---
+++ precache-manifest.3feae5e2dddb9ff8e26664c936b9da8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f89a6800be219f6ee22e5c87a109627b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "1b25196d6a9a16dd8ddb",
+    "url": "/monster-rolodex/static/css/main.0477cb3a.chunk.css"
+  },
+  {
+    "revision": "8256d7d0304953484fc5",
+    "url": "/monster-rolodex/static/js/2.f5605220.chunk.js"
+  },
+  {
+    "revision": "1b25196d6a9a16dd8ddb",
+    "url": "/monster-rolodex/static/js/main.48c0f84a.chunk.js"
+  },
+  {
+    "revision": "6f70f97b8cfdb2d06998",
+    "url": "/monster-rolodex/static/js/runtime-main.aec4d7d7.js"
+  }
+]);
